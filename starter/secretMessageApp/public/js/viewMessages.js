4eddf9eaeba06634c42892e0b638f2a62dc03a58
Kayden Moreno <kaydenmoreno143@gmail.com>
2021-07-22 17:44:20 +0000

    Independent practice

--- starter/secretMessageApp/public/js/viewMessages.js
+++ starter/secretMessageApp/public/js/viewMessages.js
@@ -1,31 +1,18 @@
-const getMessages = () => {
+document.querySelector("#viewMsg").addEventListener("click", (e) => {
+    const userPasscodeGuess = document.querySelector("#passcode").value;
     const messagesRef = firebase.database().ref();
-    messagesRef.on('value', (snapshot) => {
-        const data = snapshot.val();
-        console.log(data);
-        for (let key in data) {
-            const message = data[key];
-            console.log(message);
-        };
-    });
-}
-
-const findMessage = (myPass) => {
-    const messagesRef = firebase.database().ref();
-    messagesRef.on('value', (snapshot) => {
-        const data = snapshot.val();
-        console.log(data);
-        for (let key in data) {
-            const message = data[key];
-            console.log(message);
-            if (myPass == message.passcode) {
-                renderMessage(message);
+    messagesRef.on("value", (snapshot) => {
+            const data = snapshot.val()
+            for (let key in data) {
+                    if(data[key].passcode == userPasscodeGuess) {
+                            display(data[key]);
+                            console.log(data[key]);
+                    }
             }
-        };
-    });
+    })
+})
+
+function display(messageObject) {
+    document.querySelector("#message").innerHTML = messageObject.message;
 }
 
-document.querySelector("#viewMsg").addEventListener("click", () => {
-    const passcode = document.querySelector("#passcode").value;
-    findMessage(passcode);
-})
